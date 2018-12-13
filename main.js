a1b860895946d58251799283444403d44f50558c
Pippo Raimondi <filipporai@gmail.com>
2018-12-13 16:00:54 +0000

    changing main to reflect file naming convention change

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-var searchCompany = require('./routes/search-company.js')
-var search = require('./routes/search.js')
+var companySearchFilter = require('./routes/company-search-filter.js')
+var filters = require('./routes/filters.js')
 
-Sandbox.define('/api/v1/company/search/','POST', searchCompany.post)
+Sandbox.define('/api/v1/company/search/','POST', companySearchFilter.post)
 
-Sandbox.define('/api/v1/company/search/region/','GET', search.region)
-Sandbox.define('/api/v1/company/search/export_propensity/','GET', search.exportPropensity)
-Sandbox.define('/api/v1/company/search/market_of_interest/','GET', search.marketOfInterest)
-Sandbox.define('/api/v1/company/search/service_usage/','GET', search.serviceUsage)
-Sandbox.define('/api/v1/company/search/market_exported/','GET', search.marketExported)
+Sandbox.define('/api/v1/company/search/region/','GET', filters.region)
+Sandbox.define('/api/v1/company/search/export_propensity/','GET', filters.exportPropensity)
+Sandbox.define('/api/v1/company/search/market_of_interest/','GET', filters.marketOfInterest)
+Sandbox.define('/api/v1/company/search/service_usage/','GET', filters.serviceUsage)
+Sandbox.define('/api/v1/company/search/market_exported/','GET', filters.marketExported)
